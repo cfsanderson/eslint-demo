e0379eb197c1de197ce67d031cff2155f9f55a73
cfsanderson <calebsanderson@gmail.com>
2018-04-19 22:46:15 -0400

    Linted index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let didShowMessage = false
-
 function showMessage() {
-  // eslint-disable-next-line
-  const message = "I like logs"
+  const message = 'I like logs'
 
   for (let i = 0; i < 5; i++) {
+    // eslint-disable-next-line no-console
     console.log(message)
   }
 }
 
-showMessage();
+showMessage()
